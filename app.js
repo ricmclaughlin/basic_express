3634a154ba684170193c58941f49168c6ca781e8
ric mclaughlin <ric@mclaughlin.today>
2016-03-24 09:07:59 -0700

    add jscs & jshint

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('hello world');
 });
-app.get('/books', function(req, res){
+app.get('/books', function(req, res) {
   res.send('hello books');
 });
 
-app.listen(port, function(err){
-  console.log("server running on port " + port);
+app.listen(port, function(err) {
+  console.log('server running on port ' + port);
 })
