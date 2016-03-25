f65315e227aa38289be5b345e2ad1b4e68e37494
ric mclaughlin <ric@mclaughlin.today>
2016-03-24 18:15:27 -0700

    add router

--- app.js
+++ app.js
@@ -4,19 +4,30 @@ var handlebars = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 5000;
 
+var nav = [
+  {
+    Link: '/books',
+    Text: 'Books'
+  },
+  {
+    Link: '/authors',
+    Text: 'Author'
+  }
+];
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', '.ejs');
 
+app.use('/books', bookRouter);
 app.get('/', function (req, res) {
   res.render('index', {
     title: 'hello from handlebars',
-    list: ['a', 'b']
+    nav: nav
   });
-});
-app.get('/books', function (req, res) {
-  res.send('hello books');
 });
 
 app.listen(port, function (err) {
